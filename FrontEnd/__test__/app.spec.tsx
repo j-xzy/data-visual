6b3409ca5ee3d4a8535254d36440862934642005
whj <wu38607@gmail.com>
2018-02-15 14:07:07 +0800

    formart

--- FrontEnd/__test__/app.spec.tsx
+++ FrontEnd/__test__/app.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import  * as Enzyme from 'enzyme';
+import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import App from '../app';
 import Com from '@components/com';
